a803973b5a624e2bb9d1dc9f304257e13d406ecb
kenanosullivan <kkenandemirell@gmail.com>
2022-11-26 23:23:51 +0300

    i write anything

---
+++ src/gitLearn/gitKomut.java
@@ -0,0 +1,8 @@
+package gitLearn;
+
+public class gitKomut {
+    public static void main(String[] args) {
+        System.out.println("terminalden branch acilacak.");
+        System.out.println("merge yapilacak..");
+    }
+}
---
+++ src/gitLearn/gitMerge.java
@@ -0,0 +1,7 @@
+package gitLearn;
+
+public class gitMerge {
+    public static void main(String[] args) {
+        System.out.println("terminal üzerinden gti komutuyla local master'a merge yapilacak.");
+    }
+}
